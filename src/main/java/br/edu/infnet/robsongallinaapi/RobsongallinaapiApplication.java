701d1bcfed1ff40b8144f553f4bbd906c40cf6e3
Robsonsqn <robson.gallina@gmail.com>
2025-10-08 10:49:21 -0300

    initial commit

---
+++ src/main/java/br/edu/infnet/robsongallinaapi/RobsongallinaapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.robsongallinaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobsongallinaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobsongallinaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/robsongallinaapi/RobsongallinaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.robsongallinaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobsongallinaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
